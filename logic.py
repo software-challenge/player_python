4bae3de8420e895cced910279f406786f2ef84ac
Falcon <stu222782@mail.uni-kiel.de>
2022-08-29 20:48:37 +0200

    Simplified boilerplate

--- logic.py
+++ logic.py
@@ -1,16 +1,14 @@
 import random
 
-from socha.api.networking.player_client import IClientHandler
-from socha.api.plugin.penguins import *
-from socha.starter import Starter
+from socha import *
 
 
 class Logic(IClientHandler):
     gameState: GameState
 
     def calculate_move(self) -> Move:
-        mostFish = self.gameState.get_most_fish_moves()
-        return mostFish[random.randint(0, len(mostFish) - 1)]
+        possible_moves = self.gameState.possible_moves
+        return possible_moves[random.randint(0, len(possible_moves) - 1)]
 
     def on_update(self, state: GameState):
         self.gameState = state
@@ -20,4 +18,4 @@ class Logic(IClientHandler):
 
 
 if __name__ == "__main__":
-    Starter("Localhost", 13050, Logic())
+    Starter(logic=Logic())
---
+++ socha/__init__.py
@@ -0,0 +1,16 @@
+from socha.api.networking.player_client import IClientHandler
+from socha.api.plugin.penguins import *
+from socha.starter import Starter
+
+__all__ = [
+    'IClientHandler',
+    'Starter',
+    'Vector',
+    'Coordinate',
+    'Move',
+    'Team',
+    'Field',
+    'Board',
+    'Fishes',
+    'GameState'
+]
