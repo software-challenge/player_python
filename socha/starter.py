73dd3ca029e32f34487040bd009d20cc9d2c230f
Falcon <35808469+FalconsSky@users.noreply.github.com>
2022-09-06 16:39:14 +0200

    starter: start arguments using now argparse

--- socha/starter.py
+++ socha/starter.py
@@ -1,9 +1,9 @@
 """
 This is the main entry point for the SoCha application.
 """
+import argparse
 import datetime
 import logging
-import sys
 
 from socha.api.networking.player_client import _PlayerClient, IClientHandler
 
@@ -16,7 +16,7 @@ class Starter:
     """
 
     def __init__(self, logic: IClientHandler, host: str = "localhost", port: int = 13050, reservation: str = None,
-                 room_id: str = None, keep_alive: bool = False, write_log: bool = False):
+                 room_id: str = None, survive: bool = False, log: bool = False):
         """
         All these arguments can be overwritten, when parsed via start arguments,
         or you initialize this class with the desired values.
@@ -26,23 +26,19 @@ class Starter:
         :param port: The port of the host.
         :param reservation: Reservation code for a prepared game.
         :param room_id: Room Id the client will try to connect.
-        :param keep_alive: If True the client will keep running, even if the connection to the server is terminated.
-        :param write_log: If True the client will write a log file to the current directory.
+        :param survive: If True the client will keep running, even if the connection to the server is terminated.
+        :param log: If True the client will write a log file to the current directory.
         """
         args = self._handle_start_args()
 
-        self.host: str = host
-        self.port: int = port
-        self.reservation: str = reservation
-        self.room_id: str = room_id
-        self.keep_alive: bool = keep_alive
-        self.write_log: bool = write_log
+        self.host: str = args.host or host
+        self.port: int = args.port or port
+        self.reservation: str = args.reservation or reservation
+        self.room_id: str = args.room or room_id
+        self.keep_alive: bool = args.survive or survive
+        self.write_log: bool = args.log or log
 
-        for key, value in args.items():
-            if value:
-                setattr(self, key, value)
-
-        if write_log:
+        if self.write_log:
             now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
             logging.basicConfig(filename=f"log{now}", level=logging.INFO)
             logging.getLogger().addHandler(logging.StreamHandler())
@@ -61,47 +57,19 @@ class Starter:
 
         self.client.start()
 
-    _arguments = {
-        "help": None,
-        "host": None,
-        "port": None,
-        "reservation": None,
-        "room": None,
-        "keep_alive": None,
-        "write_log": None
-    }
-
-    def _handle_start_args(self):
-        if len(sys.argv) > 1:
-            if sys.argv[1] == "--help":
-                self._show_help()
-                sys.exit(0)
-            for i, argument in enumerate(sys.argv):
-                arg = argument.replace("--", "")
-                if arg in self._arguments:
-                    if arg == "port":
-                        self._arguments[arg] = int(sys.argv[i + 1])
-                    elif arg == "keep-alive":
-                        self._arguments[arg] = True
-                    elif arg == "write-log":
-                        self._arguments[arg] = True
-                    else:
-                        self._arguments[arg] = sys.argv[i + 1]
-        return self._arguments
-
     @staticmethod
-    def _show_help():
-        print("Usage:\n"
-              "python logic.py [--help] [--host <host>] [--port <port>] [--reservation <reservation>] "
-              "[--room <room id>] [--keep-alive] [--write-log]\n"
-              "---------------------------------------------------------\n"
-              "Options:\n"
-              "  --help                         Print this help message.\n"
-              "  --host <host>                  The host to connect to. The default is 'localhost'\n"
-              "  --port <port>                  The port of the host. The default is 13050.\n"
-              "  --reservation <reservation>    Reservation code for a prepared game.\n"
-              "  --room <room id>               Room Id the client will try to connect.\n"
-              "  --keep_alive                   If present the client will keep running, even if the connection to"
-              " the server is terminated.\n"
-              "  --write_log                    If present the client will write a log file to the current "
-              "directory.\n")
+    def _handle_start_args():
+        parser = argparse.ArgumentParser(description='All arguments are optional.', add_help=False,
+                                         epilog='Please make sure that the server is already running, '
+                                                'before you start your player.')
+        parser.add_argument('--help', action='help', help='Prints this help message.')
+        parser.add_argument('-h', '--host', help='The host to connect to. The default is \'localhost\'.')
+        parser.add_argument('-p', '--port', help='The port of the host. The default is 13050.', type=int)
+        parser.add_argument('-r', '--reservation', help='Reservation code for a prepared game.', type=str)
+        parser.add_argument('-R', '--room', help='Room Id the client will try to connect.', type=str)
+        parser.add_argument('-s', '--survive', action='store_true',
+                            help='If present the client will keep running, even if the connection to the server is '
+                                 'terminated.')
+        parser.add_argument('-l', '--log', action='store_true',
+                            help='If present the client will write a log file to the current directory.')
+        return parser.parse_args()
