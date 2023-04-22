1526e68fa8804779dce2b1a35c574c1e4d21d43b
Falcon <35808469+FalconsSky@users.noreply.github.com>
2023-04-23 00:37:11 +0200

    improved(socha): code for readability

--- socha/api/plugin/penguins/board.py
+++ socha/api/plugin/penguins/board.py
@@ -1,6 +1,8 @@
 import _pickle as pickle
 import logging
 import warnings
+from itertools import chain, takewhile
+from operator import attrgetter
 from typing import List, Union, Optional
 
 from socha.api.plugin.penguins.coordinate import HexCoordinate, Vector, CartesianCoordinate
@@ -110,12 +112,7 @@ class Board:
         """
         :return: A list of all empty fields.
         """
-        fields: List[Field] = []
-        for row in self.board:
-            for field in row:
-                if field.is_empty():
-                    fields.append(field)
-        return fields
+        return list(filter(lambda field: field.is_empty(), chain(*self.board)))
 
     def is_occupied(self, coordinates: HexCoordinate) -> bool:
         """
@@ -179,9 +176,7 @@ class Board:
         :return: The field at the given position, or None if the position is not valid.
         """
         cartesian = position.to_cartesian()
-        if self.is_valid(position):
-            return self._get_field(cartesian.x, cartesian.y)
-        return None
+        return self._get_field(cartesian.x, cartesian.y) if self.is_valid(position) else None
 
     def get_field_by_index(self, index: int) -> Field:
         """
@@ -227,10 +222,7 @@ class Board:
         :param field: The field to check for.
         :return: True if the board contains the field, False otherwise.
         """
-        for row in self.board:
-            if field in row:
-                return True
-        return False
+        return any(field in row for row in self.board)
 
     def contains_all(self, fields: List[Field]) -> bool:
         """
@@ -242,37 +234,31 @@ class Board:
         if not fields:
             return False
 
-        for field in fields:
-            if not self.contains(field):
-                return False
-        return True
+        return all(self.contains(field) for field in fields)
 
     def get_moves_in_direction(self, origin: HexCoordinate, direction: Vector, team_enum: Optional[TeamEnum] = None) \
             -> List[Move]:
         """
         Gets all moves in the given direction from the given origin.
-
         Args:
             origin: The origin of the move.
             direction: The direction of the move.
             team_enum: Team to make moves for.
-
         Returns:
                 List[Move]: List of moves that can be made in the given direction from the given index,
                             for the given team_enum
         """
-        if team_enum is None:
-            team_enum = self.get_field(origin).penguin.team_enum
+        team_enum = team_enum or self.get_field(origin).penguin.team_enum
         if not self.get_field(origin).penguin or self.get_field(origin).penguin.team_enum != team_enum:
             return []
 
-        moves = []
-        for i in range(1, self.width()):
+        def valid_destination(i):
             destination = origin.add_vector(direction.scalar_product(i))
-            if self._is_destination_valid(destination):
-                moves.append(Move(team_enum=team_enum, from_value=origin, to_value=destination))
-            else:
-                break
+            return self._is_destination_valid(destination)
+
+        moves = [Move(team_enum=team_enum, from_value=origin, to_value=origin.add_vector(direction.scalar_product(i)))
+                 for i in takewhile(valid_destination, range(1, self.width()))]
+
         return moves
 
     def _is_destination_valid(self, field: HexCoordinate) -> bool:
@@ -316,12 +302,9 @@ class Board:
         :param team: The team_enum to search for.
         :return: A list of all coordinates that are occupied by a penguin of the given team_enum.
         """
-        penguins = []
-        for row in self.board:
-            for field in row:
-                if field.penguin and field.penguin.team_enum == team:
-                    penguins.append(field.penguin)
-        return penguins
+        is_team_penguin = lambda field: field.penguin and field.penguin.team_enum == team
+        penguins = filter(is_team_penguin, (field for row in self.board for field in row))
+        return list(map(attrgetter('penguin'), penguins))
 
     def get_most_fish(self) -> List[Field]:
         """
@@ -329,13 +312,9 @@ class Board:
 
         :return: A list of Fields.
         """
-
-        fields = list(filter(lambda field_x: not field_x.is_occupied(), self.get_all_fields()))
-        fields.sort(key=lambda field_x: field_x.get_fish(), reverse=True)
-        for i, field in enumerate(fields):
-            if field.get_fish() < fields[0].get_fish():
-                fields = fields[:i]
-        return fields
+        fields = [field for field in self.get_all_fields() if not field.is_occupied()]
+        max_fish = max(fields, key=lambda field: field.get_fish()).get_fish()
+        return list(filter(lambda field: field.get_fish() == max_fish, fields))
 
     def get_board_intersection(self, other: 'Board') -> List[Field]:
         """
@@ -389,16 +368,13 @@ class Board:
     def pretty_print(self):
         print()
         for i, row in enumerate(self.board):
+            row_str = ""
             if (i + 1) % 2 == 0:
-                print(" ", end="")
-            for field in row:
-                if field.is_empty():
-                    print("~", end=" ")
-                elif field.is_occupied():
-                    print(field.get_team().value[0], end=" ")
-                else:
-                    print(field.get_fish(), end=" ")
-            print()
+                row_str += " "
+            row_str += " ".join(["~" if field.is_empty() else field.get_team().value[0] if field.is_occupied() else str(
+                field.get_fish())
+                                 for field in row])
+            print(row_str)
         print()
 
     def __eq__(self, other):
