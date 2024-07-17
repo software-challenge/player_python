13e71ecba83d9136ed798070fcdfe939da278b7e
maxblan <max.blankenfeld@outlook.de>
2024-07-17 13:03:46 +0200

    chore: Update ROUND_LIMIT constant to use usize instead of i32

--- src/plugin/constants.rs
+++ src/plugin/constants.rs
@@ -10,5 +10,5 @@ impl PluginConstants {
     pub const INITIAL_SALADS: i32 = 5;
     pub const INITIAL_CARROTS: i32 = 68;
 
-    pub const ROUND_LIMIT: i32 = 30;
+    pub const ROUND_LIMIT: usize = 30;
 }
